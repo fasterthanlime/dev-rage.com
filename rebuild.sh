2cfa85c91c128a1155b62096df1fe187914cc811
Amos Wenger <amos@official.fm>
2011-08-04 20:31:46 +0400

    Paths paths paths.

--- rebuild.sh
+++ rebuild.sh
@@ -1,12 +1,12 @@
 
-cd dev-rage
-#git pull
+cd public/comics
+git pull
 
 for img in *.png; do
   maxnumber=$(echo $img | cut -d- -f1)
 done
 maxnumber=$(expr $maxnumber - 0)
-githubprefix='https:\/\/github.com\/nddrylliog\/dev-rage\/blob\/master'
+cdnprefix='\/comics'
 
 for img in *.png; do
   number=$(echo $img | cut -d- -f1)
@@ -16,13 +16,13 @@ for img in *.png; do
   prevnum=$(if [[ $number -gt 1 ]]; then echo $(expr $number - 1); else echo '#'; fi)
   nextnum=$(if [[ $number -lt $maxnumber ]]; then echo $(expr $number + 1); else echo '#'; fi)
 
-  cat ../template.wtfbash | sed -e "s/TITLEGOESHERE/$title/g" \
-				-e "s/MAXNUMBERGOESHERE/$maxnumber/g" \
-				-e "s/NUMBERGOESHERE/$number/g" \
-				-e "s/CONTENTGOESHERE/<img src=\"${githubprefix}\/$img?raw=true\">/" \
-				-e "s/PREVGOESHERE/$prevnum/" \
-				-e "s/NEXTGOESHERE/$nextnum/" \
-			  > ../public/$number.html
+  cat ../../template.wtfbash | sed  -e "s/TITLEGOESHERE/$title/g" \
+				    -e "s/MAXNUMBERGOESHERE/$maxnumber/g" \
+				    -e "s/NUMBERGOESHERE/$number/g" \
+				    -e "s/CONTENTGOESHERE/<img id=strip src=\"${cdnprefix}\/$img\">/" \
+				    -e "s/PREVGOESHERE/$prevnum/" \
+				    -e "s/NEXTGOESHERE/$nextnum/" \
+			  > ../$number.html
 done
 
-echo "<!DOCTYPE html><html><head><meta http-equiv=refresh content='0;url=http://dev-rage.com/$maxnumber'></head></html>" > ../public/index.html
+echo "<!DOCTYPE html><html><head><meta http-equiv=refresh content='0;url=http://dev-rage.com/$maxnumber'></head></html>" > ../index.html
