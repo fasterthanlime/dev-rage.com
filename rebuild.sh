043ebcbbebdcec3cfa4b58459b5356ab872c4df2
Amos Wenger <amos@official.fm>
2011-08-04 15:18:46 +0400

    FUCK PRELOADING. Recursive iframes ftl.

--- rebuild.sh
+++ rebuild.sh
@@ -8,6 +8,6 @@ for img in *.png; do
   filename=$(echo $img | cut -d- -f2-)
   title=$(echo $filename | cut -d. -f1 | sed 's/-/ /g')
 
-  cat ../template.wtfbash | sed "s/TITLEGOESHERE/$title/g" | sed "s/NUMBERGOESHERE/$number/g" | sed "s/PREVIOUSCONTENTGOESHERE/<iframe src='\/$(expr $number - 1)'>/" | sed "s/NEXTCONTENTGOESHERE/<iframe src='\/$(expr $number + 1)'>/" | sed "s/CONTENTGOESHERE/<img src=\"https:\/\/github.com\/nddrylliog\/dev-rage\/blob\/master\/$img?raw=true\">/" > ../public/$number.html
+  cat ../template.wtfbash | sed "s/TITLEGOESHERE/$title/g" | sed "s/NUMBERGOESHERE/$number/g" | sed "s/CONTENTGOESHERE/<img src=\"https:\/\/github.com\/nddrylliog\/dev-rage\/blob\/master\/$img?raw=true\">/" > ../public/$number.html
   echo "<!DOCTYPE html><html><head><meta http-equiv=refresh content='0;url=http://dev-rage.com/$number'></head></html>" > ../public/index.html
 done
